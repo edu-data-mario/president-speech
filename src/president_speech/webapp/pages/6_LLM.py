ea2839827096db095ee076a19110401b3f3e7836
dmario24 <data.mario24@gmail.com>
2023-09-05 02:33:32 +0900

    따라하지 마라

--- src/president_speech/webapp/pages/6_LLM.py
+++ src/president_speech/webapp/pages/6_LLM.py
@@ -3,6 +3,33 @@ from streamlit_chat import message
 
 st.title("president speech bot")
 
-msg = st.text_input('LIVING FOR TODAY')
-message(msg, is_user=True)
-message("본인은 ...") 
+st.session_state.setdefault(
+    'past', [
+        'hello',
+        ]
+)
+st.session_state.setdefault(
+    'generated', [
+        {'type': 'normal', 'data': 'Line 1'},
+     ]
+)
+
+def on_input_change():
+    user_input = st.session_state.user_input
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append({'type': 'normal', 'data': user_input},)
+
+   
+with st.container():
+    st.text_input("User Input:", on_change=on_input_change, key="user_input")
+    
+chat_placeholder = st.empty()
+
+ 
+with chat_placeholder.container():    
+    for i in range(len(st.session_state['generated'])):                
+        message(st.session_state['past'][i], is_user=True)
+
+        message(
+            st.session_state['generated'][i]['data']
+        )
