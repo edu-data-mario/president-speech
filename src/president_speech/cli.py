c67f450169040994379eb6036d53ca2d5c217d89
dmario24 <data.mario24@gmail.com>
2023-09-06 17:58:30 +0900

    first deploy & use cli

--- src/president_speech/cli.py
+++ src/president_speech/cli.py
@@ -1,4 +1,5 @@
 from president_speech.db.search import title, speech
+from president_speech.db.parquet_interpreter import plot_president_word_frequency, table_president_word_frequency
 import argparse
 
 
@@ -16,5 +17,23 @@ def search():
 
     elif args.speech:
         speech(args.keyword)
+    else:
+        print("An undefined functional call has occurred.")
+
+
+def word_count():
+    parser = argparse.ArgumentParser(description="Word frequency output from previous presidential speeches")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-t", "--table", action="store_true", help="Table Format Output")
+    group.add_argument("-p", "--plot", action="store_true", help="Format Output")
+    parser.add_argument("word", type=str, help="Search word")
+
+    args = parser.parse_args()
+
+    if args.table:
+        table_president_word_frequency(args.word)
+
+    elif args.plot:
+        plot_president_word_frequency(args.word)
     else:
         print("An undefined functional call has occurred.")
