090ee2658c2765101b3d74e3bea2710783a5570f
dmario24 <data.mario24@gmail.com>
2023-09-07 15:01:35 +0900

    add bar chart

--- src/president_speech/webapp/home.py
+++ src/president_speech/webapp/home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from president_speech.db.parquet_interpreter import read_parquet
+
 markdown = '''
 ### president-speech ON-LINE
 ```
@@ -18,3 +20,15 @@ pip install president-speech==0.7.0
 '''
 
 st.markdown(markdown)
+
+df = read_parquet(use_columns=["president"])
+grouped = df.groupby("president")
+result_df = grouped.size().reset_index(name="speeches")
+
+
+st.bar_chart(
+    result_df,
+    x='president',
+    y='speeches',
+    height=200,
+)
