51139f58eafa8cbbb125cf4c33a6bb40a39d9e08
dmario24 <data.mario24@gmail.com>
2023-09-07 12:45:17 +0900

    muti page webapp

--- src/president_speech/webapp/home.py
+++ src/president_speech/webapp/home.py
@@ -1,29 +1,15 @@
 import streamlit as st
-import pandas as pd
-import president_speech.db.parquet_interpreter as pi
-
-
-df = pd.DataFrame()
-
-
-def get_df(word: str, columns: list) -> pd.DataFrame:
-    df_1 = pi.read_parquet(use_columns=columns)
-    df_r = df_1[df_1["president"].str.contains(word)]
-    return df_r
-
-
-# 검색어 입력받기
-search_word = st.text_input("검색어를 입력하세요: ")
-
-
-# 검색어를 포함하는 행을 추출
-if search_word:
-    # DataFrame 생성
-    df = get_df(search_word, ["date", "title", "president"])
-    df = df[df["president"].str.contains(search_word)]
-
-# 결과 출력
-if df.empty:
-    st.write("검색 결과가 없습니다.")
-else:
-    st.table(df)
+markdown = '''
+```
+┌─┐┬─┐┌─┐┌─┐┬┌┬┐┌─┐┌┐┌┌┬┐
+├─┘├┬┘├┤ └─┐│ ││├┤ │││ │ 
+┴  ┴└─└─┘└─┘┴─┴┘└─┘┘└┘ ┴ 
+┌─┐┌─┐┌─┐┌─┐┌─┐┬ ┬       
+└─┐├─┘├┤ ├┤ │  ├─┤       
+└─┘┴  └─┘└─┘└─┘┴ ┴       
+┌─┐┌┐┌┬  ┬┌┐┌            
+│ │││││  ││││            
+└─┘┘└┘┴─┘┴┘└┘            
+```
+'''
+st.markdown(markdown)
